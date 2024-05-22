11a28e321621877d44889c33fd02b840bb4a5ed3
LyadovOA <lyadov.oleg2011@yandex.ru>
2024-05-22 12:57:05 +0400

    Исправление 10 работы

--- main.py
+++ main.py
@@ -1,76 +1,85 @@
 import matplotlib.pyplot as plt
 import streamlit as st
-def process_data(lines):
+
+def get_group_func(lines):
     male_fares = []
     female_fares = []
 
     for line in lines:
         data = line.strip().split(",")
         sex = data[5]
-        fare = float(data[10])
-        if sex == 'male':
-            male_fares.append(fare)
-        elif sex == 'female':
-            female_fares.append(fare)
+        fare_str = data[10].strip()  # Удаляем лишние пробелы
+        if fare_str:  # Проверяем, что строка не пустая
+            fare = float(fare_str)
+            if sex == 'male':
+                male_fares.append(fare)
+            elif sex == 'female':
+                female_fares.append(fare)
 
-    male_stats = {
-        'min_fare': min(male_fares),
-        'max_fare': max(male_fares),
-        'avg_fare': sum(male_fares) / len(male_fares) if male_fares else None
-    }
+    male_avg_fare = sum(male_fares) / len(male_fares) if male_fares else 0
+    female_avg_fare = sum(female_fares) / len(female_fares) if female_fares else 0
 
-    female_stats = {
-        'min_fare': min(female_fares),
-        'max_fare': max(female_fares),
-        'avg_fare': sum(female_fares) / len(female_fares) if female_fares else None
-    }
+    male_min_fare = min(male_fares) if male_fares else 0
+    female_min_fare = min(female_fares) if female_fares else 0
 
-    return male_stats, female_stats
+    male_max_fare = max(male_fares) if male_fares else 0
+    female_max_fare = max(female_fares) if female_fares else 0
+
+    AvgFares = {'male_avg_fare': male_avg_fare, 'female_avg_fare': female_avg_fare}
+    MinFares = {'male_min_fare': male_min_fare, 'female_min_fare': female_min_fare}
+    MaxFares = {'male_max_fare': male_max_fare, 'female_max_fare': female_max_fare}
+
+    return AvgFares, MinFares, MaxFares
+
 
 with open("data.csv") as file:
     next(file)
-    male_stats, female_stats = process_data(file.readlines())
+    lines = file.readlines()
+    AvgFares, MinFares, MaxFares = get_group_func(lines)
 
-st.image('titanic.jpg')
-st.header('Данные пассажиров Титаника')
-st.write("Для просмотра данных о стоимости билетов , выберите пункт из списка ")
-selected = st.selectbox('Выберите тип стоимости', ['Средняя цена', 'Максимальная цена', 'Минимальная цена'])
 
-if selected == 'Средняя цена':
-    averageFare = [male_stats['avg_fare'], female_stats['avg_fare']]
-    sex = ['Мужчины', 'Женщины']
-    data = {'Пол': sex, 'Цена': averageFare}
-    st.table(data)
+def do_var10(lines=None):
+    st.image('titanic.jpg')
+    st.header('Данные пассажиров Титаника')
+    st.write("Для просмотра данных о стоимости билетов , выберите пункт из списка ")
+    selected = st.selectbox('Выберите тип стоимости', ['Средняя цена', 'Максимальная цена', 'Минимальная цена'])
 
-    fig = plt.figure(figsize=(10, 5))
-    plt.bar(sex, averageFare)
-    plt.xlabel("Пол")
-    plt.ylabel("Цена")
-    plt.title("Средняя стоимость билетов")
-    st.pyplot(fig)
+    if selected == 'Средняя цена':
+        averageFare = [AvgFares['male_avg_fare'], AvgFares['female_avg_fare']]
+        sex = ['Мужчины', 'Женщины']
+        data = {'Пол': sex, 'Цена': averageFare}
+        st.table(data)
 
-elif selected == 'Максимальная цена':
-    maxFare = [male_stats['max_fare'], female_stats['max_fare']]
-    sex = ['Мужчины', 'Женщины']
-    data = {'Пол': sex, 'Цена': maxFare}
-    st.table(data)
+        fig = plt.figure(figsize=(10, 5))
+        plt.bar(sex, averageFare)
+        plt.xlabel("Пол")
+        plt.ylabel("Цена")
+        plt.title("Средняя стоимость билетов")
+        st.pyplot(fig)
 
-    fig = plt.figure(figsize=(10, 5))
-    plt.bar(sex, maxFare)
-    plt.xlabel("Пол")
-    plt.ylabel("Цена")
-    plt.title("Максимальная стоимость билетов")
-    st.pyplot(fig)
+    elif selected == 'Максимальная цена':
+        maxFare = [MaxFares['male_max_fare'], MaxFares['female_max_fare']]
+        sex = ['Мужчины', 'Женщины']
+        data = {'Пол': sex, 'Цена': maxFare}
+        st.table(data)
 
-elif selected == 'Минимальная цена':
-    minFare = [male_stats['min_fare'], female_stats['min_fare']]
-    sex = ['Мужчины', 'Женщины']
-    data = {'Пол': sex, 'Цена': minFare}
-    st.table(data)
+        fig = plt.figure(figsize=(10, 5))
+        plt.bar(sex, maxFare)
+        plt.xlabel("Пол")
+        plt.ylabel("Цена")
+        plt.title("Максимальная стоимость билетов")
+        st.pyplot(fig)
 
-    fig = plt.figure(figsize=(10, 5))
-    plt.bar(sex, minFare)
-    plt.xlabel("Пол")
-    plt.ylabel("Цена")
-    plt.title("Минимальная стоимость билетов")
-    st.pyplot(fig)
+    elif selected == 'Минимальная цена':
+        minFare = [MinFares['male_min_fare'], MinFares['female_min_fare']]
+        sex = ['Мужчины', 'Женщины']
+        data = {'Пол': sex, 'Цена': minFare}
+        st.table(data)
+
+        fig = plt.figure(figsize=(10, 5))
+        plt.bar(sex, minFare)
+        plt.xlabel("Пол")
+        plt.ylabel("Цена")
+        plt.title("Минимальная стоимость билетов")
+        st.pyplot(fig)
+do_var10()
--- test_main.py
+++ test_main.py
@@ -1,40 +1,48 @@
 import pytest
-from main import process_data
-# Тест для проверки корректной обработки данных о мужчинах
-def test_process_data_male():
+from main import get_group_func
+# №1 Тест для проверки корректного подсчета средней цены для мужчин и женщин
+def test_avgFares():
     lines = [
         "1,0,3,'Braund, Mr. Owen Harris',male,22,1,0,A/5 21171,7.25,,S\n",
         "2,1,1,'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',female,38,1,0,PC 17599,71.2833,C85,C\n",
         "3,1,3,'Heikkinen, Miss. Laina',female,26,0,0,STON/O2. 3101282,7.925,,S\n"
     ]
-    male_stats, female_stats = process_data(lines)
-    assert male_stats['min_fare'] == 7.25
-    assert male_stats['max_fare'] == 7.25
-    assert male_stats['avg_fare'] == 7.25
+    avg_fares, min_fares, max_fares = get_group_func(lines)
+    assert avg_fares == {'male_avg_fare': 7.25, 'female_avg_fare': 39.60415}
 
-# Тест для проверки корректной обработки данных о женщинах
-def test_process_data_female():
+# №2 Тест для проверки корректного подсчета максимальной цены для мужчин и женщин
+def test_maxFares():
     lines = [
         "1,0,3,'Braund, Mr. Owen Harris',male,22,1,0,A/5 21171,7.25,,S\n",
         "2,1,1,'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',female,38,1,0,PC 17599,71.2833,C85,C\n",
         "3,1,3,'Heikkinen, Miss. Laina',female,26,0,0,STON/O2. 3101282,7.925,,S\n"
     ]
-    male_stats, female_stats = process_data(lines)
-    assert female_stats['min_fare'] == 7.925
-    assert female_stats['max_fare'] == 71.2833
-    assert female_stats['avg_fare'] == 39.60415
+    avg_fares, min_fares, max_fares = get_group_func(lines)
+    assert max_fares == {'male_max_fare': 7.25, 'female_max_fare': 71.2833}
 
-# Этот тест проверяет, что функция корректно обрабатывает случаи, когда для некоторых пассажиров отсутствует информация о поле.
-def test_process_data_missing_gender():
+# №3 Тест для проверки корректного подсчета минимальной цены для мужчин и женщин
+
+def test_minFares():
     lines = [
-        "1,0,3,'Braund, Mr. Owen Harris',male,22,1,0,A/5 21171,7.25,S\n",
-        "2,1,1,'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',,38,1,0,PC 17599,71.2833,C85,C\n",
+        "1,0,3,'Braund, Mr. Owen Harris',male,22,1,0,A/5 21171,7.25,,S\n",
+        "2,1,1,'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',female,38,1,0,PC 17599,71.2833,C85,C\n",
         "3,1,3,'Heikkinen, Miss. Laina',female,26,0,0,STON/O2. 3101282,7.925,,S\n"
     ]
-    male_stats, female_stats = process_data(lines)
-    assert male_stats['min_fare'] == 7.25
-    assert male_stats['max_fare'] == 7.25
-    assert male_stats['avg_fare'] == 7.25
-    assert female_stats['min_fare'] == 7.925
-    assert female_stats['max_fare'] == 7.925
-    assert female_stats['avg_fare'] == 7.925
+    avg_fares, min_fares, max_fares = get_group_func(lines)
+    assert min_fares == {'male_min_fare': 7.25, 'female_min_fare': 7.925}
+
+# №4 Тест для данных с отсутствием информации о ценах билетов для мужчин
+
+def test_noFires():
+    lines = ["1,0,3,'Braund, Mr. Owen Harris',male,22,1,0,A/5 21171,,S\n",
+        "2,1,1,'Cumings, Mrs. John Bradley (Florence Briggs Thayer)',female,38,1,0,PC 17599,71.2833,C85,C\n",
+        "3,1,3,'Heikkinen, Miss. Laina',female,26,0,0,STON/O2. 3101282,7.925,,S\n"
+    ]
+    avg_fares, min_fares, max_fares = get_group_func(lines)
+    assert min_fares == {'female_min_fare': 7.925, 'male_min_fare': 0}
+
+# №5 Тест с полным отсутствием информации
+def test_noData():
+    lines = []
+    avg_fares, min_fares, max_fares = get_group_func(lines)
+    assert avg_fares  == {'male_avg_fare': 0, 'female_avg_fare': 0}
